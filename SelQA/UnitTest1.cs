a0c61bb5eea58276e9b66896679b6a893f638746
Manny <32347232+Mlpichardo1@users.noreply.github.com>
2018-08-19 22:26:12 -0400

    Practicing alerts

--- SelQA/UnitTest1.cs
+++ SelQA/UnitTest1.cs
@@ -4,13 +4,11 @@ namespace Tests
 {
     public class Tests
     {
-    
-        // How to set an alert
-        [Test]
-        public void Test1()
-        {
-            IWebDriver driver = new FirefoxDriver();
 
+        // How to set an alert
+        public bool isAlertPresent()
+        {
+            WebDriver driver;
             try 
             {
                 driver.switchTo().alert();
@@ -19,6 +17,31 @@ namespace Tests
                    catch (Eexception e)
                    return false
                 }
+            }
+
         }
-    }
+
+        [Test]
+        public void Test1()
+        {
+            driver = new FirefoxDriver();
+
+            // Accept an alert
+            driver.switchTo().alert().Accept();
+            // Dismiss an alert
+            driver.switchTo().alert().Dismiss();
+
+            // Getting the text of the alert
+            Alert text = driver.switchTo().alert();
+            System.Console.WriteLine(text.getText());
+
+            // Selecting a Frame
+
+        }
+
+        public void Test2()
+        {
+
+        }
+    }  
 }
